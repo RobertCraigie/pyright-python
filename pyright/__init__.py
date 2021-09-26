f215010013ca3c50255fed8f2b104b68ccca52cb
Robert Craigie <robertcraigie30@gmail.com>
2021-09-26 12:05:29 +0100

    feat(package): release v0.0.10

--- pyright/__init__.py
+++ pyright/__init__.py
@@ -5,7 +5,7 @@ __title__ = 'pyright'
 __author__ = 'RobertCraigie'
 __license__ = 'MIT'
 __copyright__ = 'Copyright 2021 Robert Craigie'
-__version__ = '0.0.9'
+__version__ = '0.0.10'
 
 
 import os
