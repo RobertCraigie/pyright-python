863b9b49a3408d1646251cd22df12f5286e489c7
Robert Craigie <robertcraigie30@gmail.com>
2021-11-25 14:51:38 +0000

    chore: add test that should fail

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ import subprocess
 from pathlib import Path
 
 import pyright
+from pyright.node import maybe_decode
 
 
 VERSION_REGEX = re.compile(r'pyright (?P<version>\d+\.\d+\.\d+)')
@@ -24,14 +25,12 @@ def test_module_invocation() -> None:
 
 def test_entry_point() -> None:
     proc = subprocess.run(
-        ['pyright', 'foo'],
+        ['pyright', '--version'],
         check=True,
         stdout=subprocess.PIPE,
     )
     assert proc.returncode == 0
     output = proc.stdout.decode('utf-8')
-    print(output)
-    assert False
     match = VERSION_REGEX.match(output)
     assert match is not None
 
@@ -45,3 +44,14 @@ def test_long_arguments(tmp_path: Path) -> None:
 
     data = json.loads(result.stdout)
     assert data['generalDiagnostics'][0]['message'] == 'Type of "1" is "Literal[1]"'
+
+
+def test_argument_separator(tmp_path: Path) -> None:
+    """Ensure the npx / pyright argument separator correctly separates arguments"""
+    tmp_path.joinpath('foo.py').write_text('reveal_type(1)')
+
+    result = pyright.run('foo.py', stdout=subprocess.PIPE)
+    assert result.returncode == 0
+
+    output = maybe_decode(result.stdout)
+    assert re.match(r'File or directory .* does not exist\.', output) is None
