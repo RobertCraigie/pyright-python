e58e3ee2ccbcf4fb7f116acca5a39c77b8a0f1ba
Tim Bre <tbr@spieletipps.de>
2021-11-25 16:03:14 +0100

    fix position of `--` arg to npx (#12)
    Co-authored-by: Robert Craigie <robertcraigie30@gmail.com>

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,6 +5,7 @@ import subprocess
 from pathlib import Path
 
 import pyright
+from pyright.node import maybe_decode
 
 
 VERSION_REGEX = re.compile(r'pyright (?P<version>\d+\.\d+\.\d+)')
@@ -43,3 +44,14 @@ def test_long_arguments(tmp_path: Path) -> None:
 
     data = json.loads(result.stdout)
     assert data['generalDiagnostics'][0]['message'] == 'Type of "1" is "Literal[1]"'
+
+
+def test_argument_separator(tmp_path: Path) -> None:
+    """Ensure the npx / pyright argument separator correctly separates arguments."""
+    tmp_path.joinpath('foo.py').write_text('reveal_type(1)')
+
+    result = pyright.run('foo.py', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    assert result.returncode == 0
+
+    output = maybe_decode(result.stdout)
+    assert 'does not exist' not in output
