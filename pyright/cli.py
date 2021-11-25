cbfafbff2f3818cc90f7e295daaabf9dc5b62a95
Tim Bre <t.brendenahl@stroeermediabrands.de>
2021-11-25 15:58:13 +0100

    extract npx cmd string building for testing

--- pyright/cli.py
+++ pyright/cli.py
@@ -2,7 +2,7 @@ import os
 import sys
 import logging
 import subprocess
-from typing import List, NoReturn, Union, Any
+from typing import List, NoReturn, Union, Any, Tuple
 
 from . import node
 from .utils import env_to_bool
@@ -20,15 +20,10 @@ def main(args: List[str], **kwargs: Any) -> int:
     return run(*args, **kwargs).returncode
 
 
-def run(
-    *args: str, **kwargs: Any
-) -> Union['subprocess.CompletedProcess[bytes]', 'subprocess.CompletedProcess[str]']:
-    version = os.environ.get('PYRIGHT_PYTHON_FORCE_VERSION')
-    if version is None:
-        version = node.latest('pyright')
-
-    npx = node.version('npx')
-    if npx[0] >= 7:
+def build_cmd(
+    *args: str, pyright_version: str, npx_version: Tuple[int, ...]
+) -> List[str]:
+    if npx_version[0] >= 7:
         pre_args = ['--yes']
 
         if not env_to_bool('PYRIGHT_PYTHON_VERBOSE', default=False):
@@ -39,7 +34,22 @@ def run(
     if args and pre_args:
         pre_args = (*pre_args, '--')
 
-    return node.run('npx', *pre_args, f'pyright@{version}', *args, **kwargs)
+    return ['npx', *pre_args, f'pyright@{pyright_version}', *args]
+
+
+def run(
+    *args: str, **kwargs: Any
+) -> Union['subprocess.CompletedProcess[bytes]', 'subprocess.CompletedProcess[str]']:
+    version = os.environ.get('PYRIGHT_PYTHON_FORCE_VERSION')
+    if version is None:
+        version = node.latest('pyright')
+
+    npx = node.version('npx')
+
+    target, *cmd_args = build_cmd(*args, pyright_version=version, npx_version=npx)
+    assert target == 'npx'
+
+    return node.run(target, *cmd_args, **kwargs)
 
 
 def entrypoint() -> NoReturn:
