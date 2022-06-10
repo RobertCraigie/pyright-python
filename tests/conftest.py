4abf9b1b560298fc02630cee72bfa5a528fae3f3
Robert Craigie <robert@craigie.dev>
2022-06-10 21:56:32 +0100

    feat(cli): add support for disabling the npx version check (#77)

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,8 @@ from typing import Iterator
 
 import pytest
 
+from pyright import node
+
 
 @pytest.fixture(name='tmp_path')
 def tmp_path_fixture(tmp_path: Path) -> Iterator[Path]:
@@ -14,3 +16,19 @@ def tmp_path_fixture(tmp_path: Path) -> Iterator[Path]:
         yield tmp_path
     finally:
         os.chdir(cwd)
+
+
+@pytest.fixture(name='npx', scope='session')
+def npx_fixture() -> str:
+    return str(node._ensure_available('npx').path)  # pyright: reportPrivateUsage=false
+
+
+@pytest.fixture(autouse=True)
+def temp_env() -> Iterator[None]:
+    old = os.environ.copy()
+
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(old)
