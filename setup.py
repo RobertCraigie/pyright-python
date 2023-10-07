5cf40388f9b90254b3af92faf56dd37464b3ab93
Robert Craigie <robert@craigie.dev>
2023-10-07 12:29:40 +0100

    Remove classifier for now

--- setup.py
+++ setup.py
@@ -71,6 +71,5 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12',
     ],
 )
